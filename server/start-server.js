b49bb31736e81b9215c5eff2c4728413c35adde4
David Millet <dmillet@box.com>
2016-09-06 13:15:14 -0700

    webserver start

--- server/start-server.js
+++ server/start-server.js
@@ -1,2 +1,13 @@
 
 console.info('starting server');
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('client'));
+
+var port = 5001;
+
+app.listen(port, function() {
+	console.info('Express server started at http://localhost:' + port);
+});
